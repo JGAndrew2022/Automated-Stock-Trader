38b046d689c30d3b0c36259643e102c1b53406fb
JGAndrew2022 <85316488+JGAndrew2022@users.noreply.github.com>
2021-11-26 12:37:37 -0700

    Delete Trading_Bot directory

--- Trading_Bot/Algorithms/PointAndFigure/autoRun.bat
+++
@@ -1,4 +0,0 @@
-@echo off
-cd /D "D:\MoneyMaking\Innovation\Trading_Bot\Algorithms\PointAndFigure"
-"C:\Users\Andrew\AppData\Local\Microsoft\WindowsApps\python.exe" "D:\MoneyMaking\Innovation\Trading_Bot\Algorithms\PointAndFigure\PointAndFigure.py"
-pause

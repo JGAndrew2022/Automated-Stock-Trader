d83b4dda544163375e78b6ce5541876eafcec93a
JGAndrew2022 <85316488+JGAndrew2022@users.noreply.github.com>
2021-12-05 21:25:54 -0700

    Delete Trading_Bot directory

--- Trading_Bot/Algorithms/RSI-MACD/autorun.bat
+++
@@ -1,4 +0,0 @@
-@echo off
-cd /D "D:\MoneyMaking\Innovation\Trading_Bot\Algorithms\RSI-MACD"
-"C:\Users\Andrew\AppData\Local\Microsoft\WindowsApps\python.exe" "D:\MoneyMaking\Innovation\Trading_Bot\Algorithms\RSI-MACD\RSIandMACD.py"
-pause

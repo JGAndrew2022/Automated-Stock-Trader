9780cee20f7f24c5cdead4330ca7b2d675e155ac
JGAndrew2022 <85316488+JGAndrew2022@users.noreply.github.com>
2021-12-13 19:54:17 -0700

    Add files via upload

---
+++ Trading_Bot/Algorithms/Profit_Taking/profit.bat
@@ -0,0 +1,3 @@
+@echo off
+cd /D "D:\MoneyMaking\Innovation\Trading_Bot\Algorithms\Profit_Taking"
+"C:\Users\Andrew\AppData\Local\Microsoft\WindowsApps\python.exe" "D:\MoneyMaking\Innovation\Trading_Bot\Algorithms\Profit_Taking\Profit_Taking.py"
